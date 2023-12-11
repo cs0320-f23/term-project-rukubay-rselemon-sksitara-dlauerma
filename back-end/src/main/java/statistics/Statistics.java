022a86b4f604f6b00b96a71997a6a7d036435f79
Siddarth Sitaraman <siddusitaraman@gmail.com>
2023-12-11 14:27:31 -0500

    feat: update to stats testing

--- back-end/src/main/java/statistics/Statistics.java
+++ back-end/src/main/java/statistics/Statistics.java
@@ -1,26 +1,28 @@
 package statistics;
 
-public class Statistics{
-  private List<int> users;
-  private List<float> overlaps;
+import java.util.ArrayList;
+import java.util.List;
 
-  public List<int> getUsers(){
+public class Statistics{
+  private List<Integer> users;
+  private List<Float> overlaps;
+
+  public List<Integer> getUsers(){
     return users;
   }
-  public List<float> getOverlaps(){
-    return overlaps;
+  public List<Float> getOverlaps() {return overlaps;}
 
-  public float computeOverlap(List<String> user1, List<String> user2){
+  public float computeOverlap(List<String> user1, List<String> user2) {
     float sum = 0;
     for (int i = 0; i < user1.size(); i++){
       if (user2.contains(user1.get(i))){
         sum++;
       }
     }
-    return sum / user1.size();
+    return (!user1.isEmpty() && !user2.isEmpty()) ? sum / ((float)(user1.size() + user2.size()) / 2.f) : 0;
   }
 
-  public float computeWeightedMatch(List<float> overlaps, List<float> weights){
+  public float computeWeightedMatch(List<Float> overlaps, List<Float> weights){
     float sum = 0;
     for (int i = 0; i < overlaps.size(); i++){
       sum += overlaps.get(i) * weights.get(i);
@@ -28,7 +30,7 @@ public class Statistics{
     return sum;
   }
 
-  public List<float> rankedList(List<String> user, List<List<String>> friends) {
+  public void rankedList(List<String> user, List<List<String>> friends) {
     for (int i = 0; i < user.size(); i++) {
       overlaps.add(computeOverlap(user, friends.get(i)));
       users.add(i);

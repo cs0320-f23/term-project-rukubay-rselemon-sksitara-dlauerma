e6fa5afe7fd809c6a698e8465a11f2198b9851e5
davidlauerman <david_lauerman@brown.edu>
2023-12-15 01:43:05 -0500

    feat: implemented terms and genres

--- back-end/src/main/java/datatypes/ourUser.java
+++ back-end/src/main/java/datatypes/ourUser.java
@@ -1,34 +1,87 @@
 package datatypes;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+
 import se.michaelthelin.spotify.model_objects.specification.Artist;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 
 public class ourUser {
   private final String username;
   private final String password;
-  private List<Artist> topArtists;
-  private List<Track> topSongs;
+  private List<Artist> topArtistsShort;
+  private List<Artist> topArtistsMed;
+  private List<Artist> topArtistsLong;
+  private List<Track> topSongsShort;
+  private List<Track> topSongsMed;
+  private List<Track> topSongsLong;
+  private HashMap<String, Integer> topGenresShort;
+  private HashMap<String, Integer> topGenresMed;
+  private HashMap<String, Integer> topGenresLong;
   public ourUser(String username, String password) {
     this.username = username;
     this.password = password;
   }
 
+  public boolean checkPassword(String attempt) {
+    return Objects.equals(attempt, password);
+  }
   public String getUsername() {
     return username;
   }
 
-  public List<Artist> getTopArtists() {
-    return topArtists;
+  public List<Artist> getTopArtistsShort() {
+    return topArtistsShort;
   }
-
-  public void setTopArtists(List<Artist> artists) {
-    topArtists = artists;
+  public void setTopArtistsShort(List<Artist> artists) {
+    topArtistsShort = artists;
   }
-  public List<Track> getTopTracks() {
-    return topSongs;
+  public List<Artist> getTopArtistsMed() {
+    return topArtistsMed;
   }
-
-  public void setTopTracks(List<Track> topSongs) {
-    topSongs = topSongs;
+  public void setTopArtistsMed(List<Artist> artists) {
+    topArtistsMed = artists;
+  }
+  public List<Artist> getTopArtistsLong() {
+    return topArtistsLong;
+  }
+  public void setTopArtistsLong(List<Artist> artists) {
+    topArtistsLong = artists;
+  }
+  public List<Track> getTopTracksShort() {
+    return topSongsShort;
+  }
+  public void setTopTracksShort(List<Track> songs) {
+    topSongsShort = songs;
+  }
+  public List<Track> getTopTracksMed() {
+    return topSongsMed;
+  }
+  public void setTopTracksMed(List<Track> songs) {
+    topSongsMed = songs;
+  }
+  public List<Track> getTopTracksLong() {
+    return topSongsLong;
+  }
+  public void setTopTracksLong(List<Track> songs) {
+    topSongsLong = songs;
+  }
+  public HashMap<String, Integer> getTopGenreShort() {
+    return topGenresShort;
+  }
+  public void setTopGenreShort(HashMap<String, Integer> genres) {
+    topGenresShort = genres;
+  }
+  public HashMap<String, Integer> getTopGenreMed() {
+    return topGenresMed;
+  }
+  public void setTopGenreMed(HashMap<String, Integer> genres) {
+    topGenresMed = genres;
+  }
+  public HashMap<String, Integer> getTopGenreLong() {
+    return topGenresLong;
+  }
+  public void setTopGenreLong(HashMap<String, Integer> genres) {
+    topGenresLong = genres;
   }
 }
--- back-end/src/main/java/handler/MakeUserHandler.java
+++ back-end/src/main/java/handler/MakeUserHandler.java
@@ -39,46 +39,89 @@ public class MakeUserHandler implements Route {
         String password = request.queryParams("password");
         ourUser newUser = new ourUser(username, password);
 
-//        GetCurrentUsersProfileRequest getCurrentUsersProfileRequest = spotifyApi.getCurrentUsersProfile()
-//                .build();
-//        try {
-//            User user = getCurrentUsersProfileRequest.execute();
-//            String spotifyUsername = user.getDisplayName();
-//
-//        } catch (IOException | SpotifyWebApiException | ParseException e) {
-//            e.printStackTrace();
-//        }
-
-
-        GetUsersTopArtistsRequest getUsersTopArtistsRequest = this.spotifyApi.getUsersTopArtists()
+        // create the requests for short, medium, and long term
+        GetUsersTopArtistsRequest getUsersTopArtistsRequestShort = this.spotifyApi.getUsersTopArtists()
+                .time_range("short_term")
+                .limit(50)
+                .build();
+        GetUsersTopArtistsRequest getUsersTopArtistsRequestMed = this.spotifyApi.getUsersTopArtists()
                 .time_range("medium_term")
                 .limit(50)
                 .build();
+        GetUsersTopArtistsRequest getUsersTopArtistsRequestLong = this.spotifyApi.getUsersTopArtists()
+                .time_range("long_term")
+                .limit(50)
+                .build();
 
+        // fetch all of the artists and store them in the user's profile
         try {
-            Paging<Artist> artists = getUsersTopArtistsRequest.execute();
-            newUser.setTopArtists(List.of(artists.getItems()));
-            //System.out.println(newUser.getTopArtists().get(0).getName());
-            //responseMap.put("artists", artists.getItems());
+            Paging<Artist> artistsShort = getUsersTopArtistsRequestShort.execute();
+            newUser.setTopArtistsShort(List.of(artistsShort.getItems()));
+            Paging<Artist> artistsMed = getUsersTopArtistsRequestMed.execute();
+            newUser.setTopArtistsMed(List.of(artistsMed.getItems()));
+            Paging<Artist> artistsLong = getUsersTopArtistsRequestLong.execute();
+            newUser.setTopArtistsLong(List.of(artistsLong.getItems()));
             responseMap.put("result", "success");
         } catch (Exception e) {
             responseMap.put("result", "failure");
             e.printStackTrace();
         }
 
-        GetUsersTopTracksRequest getUsersTopTracksRequest = this.spotifyApi.getUsersTopTracks()
+        // setting the track requests
+        GetUsersTopTracksRequest getUsersTopTracksRequestShort = this.spotifyApi.getUsersTopTracks()
+                .time_range("short_term")
+                .limit(50)
+                .offset(0)
+                .build();
+        GetUsersTopTracksRequest getUsersTopTracksRequestMed = this.spotifyApi.getUsersTopTracks()
                 .time_range("medium_term")
                 .limit(50)
                 .offset(0)
                 .build();
+        GetUsersTopTracksRequest getUsersTopTracksRequestLong = this.spotifyApi.getUsersTopTracks()
+                .time_range("long_term")
+                .limit(50)
+                .offset(0)
+                .build();
 
+        // executing the requests and storing them
         try {
-            Paging<Track> tracks = getUsersTopTracksRequest.execute();
-            newUser.setTopTracks(List.of(tracks.getItems()));
-            System.out.println(newUser.getTopArtists().get(0).getName());
+            Paging<Track> tracksShort = getUsersTopTracksRequestShort.execute();
+            newUser.setTopTracksShort(List.of(tracksShort.getItems()));
+            Paging<Track> tracksMed = getUsersTopTracksRequestMed.execute();
+            newUser.setTopTracksMed(List.of(tracksMed.getItems()));
+            Paging<Track> tracksLong = getUsersTopTracksRequestLong.execute();
+            newUser.setTopTracksLong(List.of(tracksLong.getItems()));
         } catch (Exception e) {
             responseMap.put("result", "failure");
             e.printStackTrace();
+        }
+
+        // creating maps of top genres for short term
+        newUser.setTopGenreShort(new HashMap<>());
+        for (Artist artist : newUser.getTopArtistsShort()) {
+            for (String genre : artist.getGenres()) {
+                int count = newUser.getTopGenreShort().getOrDefault(genre, 0);
+                newUser.getTopGenreShort().put(genre, count);
+            }
+        }
+
+        // repeat for medium term
+        newUser.setTopGenreMed(new HashMap<>());
+        for (Artist artist : newUser.getTopArtistsMed()) {
+            for (String genre : artist.getGenres()) {
+                int count = newUser.getTopGenreMed().getOrDefault(genre, 0);
+                newUser.getTopGenreMed().put(genre, count);
+            }
+        }
+
+        // once more for long term
+        newUser.setTopGenreLong(new HashMap<>());
+        for (Artist artist : newUser.getTopArtistsLong()) {
+            for (String genre : artist.getGenres()) {
+                int count = newUser.getTopGenreLong().getOrDefault(genre, 0);
+                newUser.getTopGenreLong().put(genre, count);
+            }
         }
 
         return adapter.toJson(responseMap);
